25e78f6e6fac564d47e3bd86badaad7eb4f4bb5e
kodir78 <kodir.petani@gmail.com>
2020-07-07 15:05:16 +0800

    install livewire

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+var Turbolinks = require("turbolinks")
+Turbolinks.start()
+
+Turbolinks.setProgressBarDelay(1)
+
+$(document).on('turbolinks:load', function (){
+  //dropdown menu
+  $('.dropdown-toggle').dropdown();
+
+});
